e3e46da7892c5234633390e855d8303e7a9cd982
Olesia Zaiats <olesia.zayats@gmail.com>
2022-12-11 21:26:31 -0600

    Play with loops

--- src/com/playground/loop/LoopPlayer.java
+++ src/com/playground/loop/LoopPlayer.java
@@ -1,11 +1,16 @@
 package com.playground.loop;
 
+import java.util.*;
+
 public class LoopPlayer {
 
     public static void main(String[] args) {
         printTriangle(5);
         System.out.println();
         printTriangleBackwards(5);
+        iterateCollection(List.of(1, 2, 3, 4, 5));
+        iterateStringCollection(Set.of("dog", "cat", "bunny", "mouse"));
+        playWithWhileLoop();
     }
 
     private static void printTriangle(int dimension) {
@@ -24,6 +29,26 @@ public class LoopPlayer {
                 System.out.print("* ");
             }
             System.out.println();
+        }
+    }
+
+    private static void iterateCollection(Collection<Integer> collection) {
+        for (Integer elementOfCollection : collection) {
+            System.out.println(elementOfCollection);
+        }
+    }
+
+    private static void iterateStringCollection(Set<String> stringSet) {
+        for (String elementOfStringSet : stringSet) {
+            System.out.println(elementOfStringSet);
+        }
+    }
+
+    private static void playWithWhileLoop() {
+        List<String> stringList = new ArrayList<>(List.of("Olesia", "Alex", "Ivan", "Iryna"));
+        Iterator<String> iterator = stringList.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
         }
     }
 
