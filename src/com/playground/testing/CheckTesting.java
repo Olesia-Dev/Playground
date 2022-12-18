1a4e53f92edc4c2b35d6dfccbee955dc2995267b
Olesia Zaiats <olesia.zayats@gmail.com>
2022-12-17 22:37:40 -0600

    Testing with JUnit

---
+++ src/com/playground/testing/CheckTesting.java
@@ -0,0 +1,32 @@
+package com.playground.testing;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class CheckTesting {
+
+    @Test
+    public void stringComparison() {
+        String stringToCompare = "Merry Christmas!";
+        assertEquals("Merry Christmas!", stringToCompare);
+    }
+
+    @Test
+    public void testSum() {
+        Mathematics mathematics = new Mathematics();
+        int result = mathematics.sumTwoNumbers(25, 31);
+
+        assertEquals(56, result);
+    }
+
+    @Test
+    public void testSumOfNegativeNumbers() {
+        Mathematics mathematics = new Mathematics();
+        int result = mathematics.sumTwoNumbers(-25, -31);
+
+        assertEquals(-56, result);
+    }
+
+}
---
+++ src/com/playground/testing/Mathematics.java
@@ -0,0 +1,10 @@
+package com.playground.testing;
+
+public class Mathematics {
+
+    public int sumTwoNumbers(int first, int second) {
+        int sum = first + second;
+        return sum;
+    }
+
+}
