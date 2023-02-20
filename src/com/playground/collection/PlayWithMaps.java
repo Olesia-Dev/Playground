7930dc4f1347426915387f95cb67042eb5d558a3
Olesia Zaiats <olesia.zayats@gmail.com>
2023-02-20 13:32:36 -0600

    Play with Hash Functions and HashCode

--- src/com/playground/collection/PlayWithMaps.java
+++ src/com/playground/collection/PlayWithMaps.java
@@ -2,10 +2,20 @@ package com.playground.collection;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class PlayWithMaps {
 
     public static void main(String[] args) {
+        Map<Person, Diamond> map = new HashMap<>();
+        map.put(new Person("Olesia"), new Diamond("Ukrainian Diamond"));
+
+        System.out.println(new Person("Olesia").hashCode());
+        System.out.println(new Person("Olesia").hashCode());
+        System.out.println(map.get(new Person("Olesia")));
+    }
+
+    public static void maps() {
         Map<Integer, Person> map = new HashMap<>();
         map.put(1, new Person("Alex"));
         map.put(2, new Person("Alexa"));
@@ -26,7 +36,34 @@ public class PlayWithMaps {
         System.out.println(map.values());
     }
 
-    record Person(String name) {
+    static class Person {
+        String name;
+
+        public Person(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Person person)) return false;
+            return Objects.equals(name, person.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name);
+        }
+    }
+
+    record Diamond(String name) {
     }
 
 }
