29ddc408f6cb9994788621f0b461f299704f90bc
Olesia Zaiats <olesia.zayats@gmail.com>
2023-03-14 16:21:56 -0500

    JavaStreams

--- src/com/playground/streams/JavaStreams.java
+++ src/com/playground/streams/JavaStreams.java
@@ -1,7 +1,8 @@
 package com.playground.streams;
 
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.IntSummaryStatistics;
+import java.util.stream.IntStream;
 
 public class JavaStreams {
 
@@ -39,11 +40,43 @@ public class JavaStreams {
 //                .sorted()
 //                .forEach(System.out::println);
 
-//        6. Average of squares of an int array
-        Arrays.stream(new int[]{2, 4, 6, 8, 10, 12, 14})
-                .map(x -> x * x)
-                .average()
-                .ifPresent(System.out::println);
+////        6. Average of squares of an int array
+//        Arrays.stream(new int[]{2, 4, 6, 8, 10, 12, 14})
+//                .map(x -> x * x)
+//                .average()
+//                .ifPresent(System.out::println);
+
+//        7. Stream from List, filter and print
+//        List<String> people = Arrays.asList("Olesia", "Oleksandr", "Oliver", "Anna", "Iryna", "lina");
+//        people
+//                .stream()
+//                .map(String::toLowerCase)
+//                .filter(x -> x.startsWith("l"))
+//                .forEach(System.out::println);
+
+//        8. Stream rows from text file, sort, filter, and print
+//        Stream<String> bands = Files.lines(Paths.get("bands.txt"));
+//        bands
+//                .sorted()
+//                .filter(x -> x.length() > 13)
+//                .forEach(System.out::println);
+//        bands.close();
+
+////        9. Stream rows from text file and save to List
+//        List<String> bands2 = Files.lines(Paths.get("bands.txt"))
+//                .filter(x -> x.contains("jit"))
+//                .collect(Collectors.toList());
+//        bands2.forEach(x -> System.out.println(x));
+
+//        10. Reduction - sum
+//        double total = Stream.of(7.3, 1.8, 4.5)
+//                .reduce(0.0, (Double a, Double b) -> a + b);
+//        System.out.println("Total = " + total);
+
+//        11. Reduction - summary statistics
+        IntSummaryStatistics summary = IntStream.of(7, 2, 19, 88, 73, 4, 10)
+                .summaryStatistics();
+        System.out.println(summary);
     }
 
 }
