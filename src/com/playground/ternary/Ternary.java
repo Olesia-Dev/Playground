f3f6e1e145f6a4ba0235c123bfb3f93c809b45ad
Olesia Zaiats <olesia.zayats@gmail.com>
2023-03-28 12:48:55 -0500

    Ternary operator

---
+++ src/com/playground/ternary/Ternary.java
@@ -0,0 +1,14 @@
+package com.playground.ternary;
+
+public class Ternary {
+
+    public static void main(String[] args) {
+        int n1 = 27, n2 = 31, max;
+        System.out.println("First number: " + n1);
+        System.out.println("Second number: " + n2);
+
+        max = Math.max(n1, n2);
+        System.out.println("Maximum is = " + max);
+    }
+
+}
