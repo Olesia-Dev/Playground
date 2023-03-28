c8760d5ef27b1d6079ee3a9e166b9b2d604f071b
Olesia Zaiats <olesia.zayats@gmail.com>
2023-03-28 13:01:26 -0500

    Assignment operator

---
+++ src/com/playground/operators/Assignment.java
@@ -0,0 +1,15 @@
+package com.playground.operators;
+
+public class Assignment {
+
+    public static void main(String[] args) {
+        int num;
+        String name;
+
+        num = 31;
+        name = "Olesia Zaiats";
+        System.out.println("Number is assignment: " + num);
+        System.out.println("Name is assignment: " + name);
+    }
+
+}
