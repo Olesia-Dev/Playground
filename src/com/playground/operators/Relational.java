fb3c99efaf722e2cfe210da80efe0a084f7bbe73
Olesia Zaiats <olesia.zayats@gmail.com>
2023-03-28 14:13:10 -0500

    Relational operator

---
+++ src/com/playground/operators/Relational.java
@@ -0,0 +1,14 @@
+package com.playground.operators;
+
+public class Relational {
+
+    public static void main(String[] args) {
+        int var1 = 31, var2 = 27, var3 = 6;
+        System.out.println("Var1 = " + var1);
+        System.out.println("Var2 = " + var2);
+        System.out.println("Var3 = " + var3);
+        System.out.println("var1 == var2: " + (var1 == var2));
+        System.out.println("var1 == var3: " + (var1 == var3));
+    }
+
+}
