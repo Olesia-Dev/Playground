b12b3cd6fda152239337bc447c59327759721a89
Olesia Zaiats <olesia.zayats@gmail.com>
2023-03-30 17:10:16 -0500

    Shift operator

---
+++ src/com/playground/operators/Shift.java
@@ -0,0 +1,11 @@
+package com.playground.operators;
+
+public class Shift {
+
+    public static void main(String[] args) {
+        int number = 4;
+        int result = number << 4;
+        System.out.println(result);
+    }
+
+}
