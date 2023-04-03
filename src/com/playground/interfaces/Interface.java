81abaa18752efe0e42a40c76fe5986333503e12c
Olesia Zaiats <olesia.zayats@gmail.com>
2023-04-03 16:56:55 -0500

    Interface implementation

---
+++ src/com/playground/interfaces/Interface.java
@@ -0,0 +1,9 @@
+package com.playground.interfaces;
+
+public interface Interface {
+
+    final int bd = 31;
+
+    void birthday();
+
+}
---
+++ src/com/playground/interfaces/Test.java
@@ -0,0 +1,16 @@
+package com.playground.interfaces;
+
+class Test implements Interface {
+
+    public void birthday() {
+        System.out.println("Olesia");
+    }
+
+    public static void main(String[] args) {
+
+        Test test = new Test();
+        test.birthday();
+        System.out.println(bd);
+    }
+
+}
