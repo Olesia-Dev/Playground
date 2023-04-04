21412658a4223fdc149c7ed6f1c670a6da89a288
Olesia Zaiats <olesia.zayats@gmail.com>
2023-04-04 15:56:33 -0500

    Instance Variables

--- src/com/playground/Main.java
+++ src/com/playground/Main.java
@@ -1,4 +1,14 @@
 package com.playground;
 
+import com.playground.variables.InstanceExample;
+
 public class Main {
+
+    public static void main(String[] args) {
+        InstanceExample name = new InstanceExample();
+        System.out.println("Student name is: " + name.student);
+        System.out.println("Default value for int is: " + name.i);
+        System.out.println("Default value for Integer is: " + name.I);
+    }
+
 }
---
+++ src/com/playground/variables/InstanceExample.java
@@ -0,0 +1,13 @@
+package com.playground.variables;
+
+public class InstanceExample {
+
+    public String student;
+    public int i;
+    public Integer I;
+
+    public InstanceExample() {
+        this.student = "Olesia Zaiats";
+    }
+
+}
